7201e550060790bddbce0d662f90cf26b8636cba
Shanza Naveed <shanzanaveed005@gmail.com>
2026-05-24 00:55:55 +0500

    destination.cs#

--- AITourismPlanner/Models/Destination.cs
+++ AITourismPlanner/Models/Destination.cs
@@ -7,9 +7,10 @@
         public string Name { get; set; }
 
 
-        public string City { get; set;  
+        public string City
+        {
+            get; set;
         }
-
         public string Country { get; set; }
 
         public string Description { get; set;

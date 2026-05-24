8e77e01386f703d174ae0ba73807e0e002c5c778
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-05-24 15:16:06 +0500

    Models files

---
+++ AITourismPlanner/Models/Destination.cs
@@ -0,0 +1,17 @@
+﻿namespace AITourismPlanner.Models
+{
+    public class Destination
+    {
+        public int DestinationId { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal EstimatedCost { get; set; }
+    }
+}
---
+++ AITourismPlanner/Models/Hotel.cs
@@ -0,0 +1,15 @@
+﻿namespace AITourismPlanner.Models
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+
+        public string HotelName { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal PricePerNight { get; set; }
+
+        public decimal StarRating { get; set; }
+    }
+}

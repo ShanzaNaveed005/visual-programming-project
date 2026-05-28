3480d2af8ea2772006cf0f95d2f2a85c078af1a1
Shanza Naveed <shanzanaveed005@gmail.com>
2026-05-28 23:33:48 +0500

    fixed hotel booking functionality

---
+++ AITourismPlanner/Controllers/HotelsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AITourismPlanner.Controllers
+{
+    public class HotelsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
